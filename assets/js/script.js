5abad859714397d5b83d323dca07b481f5b5624e
rochelledavis <96760168+rochelledavis@users.noreply.github.com>
2022-02-28 14:25:41 -0700

    html

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}'
+var secondaryUrl = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}'
+var key = "853a9458d442456e0769ca52671f8c89"
+
+
+
+//determine how to switch a city search to latitude and longitude...do I have the variables/key correct?
+//connect search input to api call when 'search' button is clicked
+//display clickable buttons from search history results. 
+    //When clicked display current and future conditions. 
+//display city name, date, icon, temperature, humidity, wind speed, UV Index, current and future conditions for that city.
+//UV Index presented with color indicating whether conditions are favorable, moderate, or severe.
+//Display 5 day forcast with date, icon, temperature, wind speed, and humidity
+
